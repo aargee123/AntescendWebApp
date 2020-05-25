6599c4dbe1ad1513aa25c4f82d4f1409c66eebb3
aargee123 <ram.gumber1456@gmail.com>
2020-05-25 15:18:43 +0530

    Updates

---
+++ precache-manifest.e5dcacc7a0a8478188d34ea809aeaa73.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae468b9e8fa7510dc636a04679456d2f",
+    "url": "/AntescendWebApp/index.html"
+  },
+  {
+    "revision": "0779655d3cfcf174235b",
+    "url": "/AntescendWebApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "31037a09abdcecf3f038",
+    "url": "/AntescendWebApp/static/css/main.433ae97a.chunk.css"
+  },
+  {
+    "revision": "0779655d3cfcf174235b",
+    "url": "/AntescendWebApp/static/js/2.2937d8ae.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/AntescendWebApp/static/js/2.2937d8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31037a09abdcecf3f038",
+    "url": "/AntescendWebApp/static/js/main.c6495688.chunk.js"
+  },
+  {
+    "revision": "edd3d241bfa54554f964",
+    "url": "/AntescendWebApp/static/js/runtime-main.53f53b15.js"
+  },
+  {
+    "revision": "a0d02930b6f072ed06c78cdb9eb6ff0a",
+    "url": "/AntescendWebApp/static/media/InstagramLogo.a0d02930.png"
+  },
+  {
+    "revision": "d27cd1ba5a89a7010d179e630788b929",
+    "url": "/AntescendWebApp/static/media/aastha.d27cd1ba.jpg"
+  },
+  {
+    "revision": "0d31601ee3efca6b3b59679860eaad9c",
+    "url": "/AntescendWebApp/static/media/ayush.0d31601e.JPEG"
+  },
+  {
+    "revision": "9d114b33471e74ab9a83c2d84917f5e1",
+    "url": "/AntescendWebApp/static/media/ayushg.9d114b33.JPEG"
+  },
+  {
+    "revision": "e3c441c561ec0b02a9577cffeab9a537",
+    "url": "/AntescendWebApp/static/media/karan.e3c441c5.JPEG"
+  },
+  {
+    "revision": "ff2c302963bf9648352a03f992605185",
+    "url": "/AntescendWebApp/static/media/kushal.ff2c3029.JPEG"
+  },
+  {
+    "revision": "1ed79fd91977accf8f0fc17b309863c3",
+    "url": "/AntescendWebApp/static/media/rahul.1ed79fd9.JPEG"
+  },
+  {
+    "revision": "b6c6c07652655cf372d0008beb383b66",
+    "url": "/AntescendWebApp/static/media/shataksh.b6c6c076.JPEG"
+  }
+]);
