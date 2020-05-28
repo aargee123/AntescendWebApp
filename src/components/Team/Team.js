80c73b4f2acae21c72b39ee26aa4087260642967
aargee123 <ram.gumber1456@gmail.com>
2020-05-28 12:16:50 +0530

    updating again

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import kushal from './kushal.JPEG';
 import ayush from './ayush.JPEG';
-import aastha from './aastha.jpg';
 import rahul from './rahul.JPEG';
 import karan from './karan.JPEG';
 import shataksh from './shataksh.JPEG';
@@ -18,39 +16,21 @@ const Team = () => {
                 <div className="tc">
                     <img src = {ayush} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Ayush Yadav"/>
                     <h1 className="f4 mb2">Ayush Yadav</h1>
-                    <h2 className="f5 fw4 mt0">Founder & CEO (Chief Executive Officer)</h2>
-                    <a href ='https://www.linkedin.com/in/ayush-yadav-a1b39115b/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
-                </div>
-            </article>
-            <article className="w5 design br3 pa2 pa4-ns mv3 mh3 grow pointer shadow-5">
-                <div className="tc">
-                    <img src = {kushal} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Kushal Ranga"/>
-                    <h1 className="f4 mb2">Kushal Ranga</h1>
-                    <h2 className="f5 fw4 mt0">Co-Founder</h2>
-                    <a href ='https://www.linkedin.com/in/kushal-ranga-86008315a/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
+                    <h2 className="f5 fw4 mt0">Founder</h2>
+                    <a  href ='https://www.linkedin.com/in/ayush-yadav-a1b39115b/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
             <article className="w5 design br3 pa2 pa4-ns mv3 mh3 grow pointer shadow-5">
                 <div className="tc">
                     <img src = {yuvraj} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Yuvraj Joshi"/>
                     <h1 className="f4 mb2">Yuvraj Joshi</h1>
-                    <h2 className="f5 fw4 mt0">CTO (Chief Technical Officer)</h2>
-                    <a href ='https://www.linkedin.com/in/yuvraj-joshi-7229631aa/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
-                </div>
-            </article>
-            <article className="w5 design br3 pa2 pa4-ns mv3 mh3 grow pointer shadow-5">
-                <div className="tc">
-                    <img src = {aastha} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Aastha Bhardwaj"/>
-                    <h1 className="f4 mb2">Aastha Bhardwaj</h1>
-                    <h2 className="f5 fw4 mt0">CHRO(Chief Human Resource Officer)</h2>
-                    <a href ='https://www.linkedin.com/in/aastha-bhardwaj-4754571a8/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
+                    <a className = 'mv3' href ='https://www.linkedin.com/in/yuvraj-joshi-7229631aa/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
             <article className="w5 design br3 pa2 pa4-ns mv3 mh3 grow pointer shadow-5">
                 <div className="tc">
                     <img src = {rahul} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Rahul Nath"/>
                     <h1 className="f4 mb2">Rahul Nath</h1>
-                    <h2 className="f5 fw4 mt0">COO (Chief Operating Officer)</h2>
                     <a href ='https://www.linkedin.com/in/rahul-nath-b22943178/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
@@ -58,7 +38,6 @@ const Team = () => {
                 <div className="tc">
                     <img src = {karan} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Karan Raina"/>
                     <h1 className="f4 mb2">Karan Raina</h1>
-                    <h2 className="f5 fw4 mt0">CMO (Chief Marketing Officer)</h2>
                     <a href ='https://www.linkedin.com/in/karan-raina-510839169/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
@@ -66,7 +45,6 @@ const Team = () => {
                 <div className="tc">
                     <img src = {shataksh} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Shataksh Bhardwaj"/>
                     <h1 className="f4 mb2">Shataksh Bhardwaj</h1>
-                    <h2 className="f5 fw4 mt0">Cloud Computing</h2>
                     <a href ='https://www.linkedin.com/in/shataksh-bhardwaj-097537170/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
@@ -74,7 +52,6 @@ const Team = () => {
                 <div className="tc">
                     <img src = {ram} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Ram Gumber"/>
                     <h1 className="f4 mb2">Ram Gumber</h1>
-                    <h2 className="f5 fw4 mt0">Full Stack Developer</h2>
                     <a href ='https://www.linkedin.com/in/ram-gumber-253798187/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
@@ -82,7 +59,6 @@ const Team = () => {
                 <div className="tc">
                     <img src = {ayushg} className="br-100 h4 w4 dib ba b--black-05 pa2" alt="Ayush Giri"/>
                     <h1 className="f4 mb2">Ayush Giri</h1>
-                    <h2 className="f5 fw4 mt0">Front-End Developer</h2>
                     <a href ='https://www.linkedin.com/in/ayush-giri-270187195/'><img className = 'dim' alt = 'instagram' src = {LinkedIcon} height = '25px' width = '30px'/></a>
                 </div>
             </article>
