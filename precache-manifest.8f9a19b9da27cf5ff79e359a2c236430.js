b04ca5a4afaa6ce80970413536d39cd8e4d25be1
aargee123 <ram.gumber1456@gmail.com>
2020-05-29 13:15:00 +0530

    Updates

---
+++ precache-manifest.8f9a19b9da27cf5ff79e359a2c236430.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b5804c69f44093f0188aa80d6e435b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6176c42f384e7892d182",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d14a332e40cb4775e2af",
+    "url": "/static/css/main.07f29834.chunk.css"
+  },
+  {
+    "revision": "6176c42f384e7892d182",
+    "url": "/static/js/2.4377a3e6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.4377a3e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14a332e40cb4775e2af",
+    "url": "/static/js/main.3d4f3730.chunk.js"
+  },
+  {
+    "revision": "e355d0bc887d2521533c",
+    "url": "/static/js/runtime-main.469b8bf9.js"
+  },
+  {
+    "revision": "42990140a6eec0476b940cd439287806",
+    "url": "/static/media/Background.42990140.jpg"
+  },
+  {
+    "revision": "a0d02930b6f072ed06c78cdb9eb6ff0a",
+    "url": "/static/media/InstagramLogo.a0d02930.png"
+  },
+  {
+    "revision": "e059d7fd2d095cf1812365b335f7b8b9",
+    "url": "/static/media/Yuvraj.e059d7fd.png"
+  },
+  {
+    "revision": "0d31601ee3efca6b3b59679860eaad9c",
+    "url": "/static/media/ayush.0d31601e.JPEG"
+  },
+  {
+    "revision": "9d114b33471e74ab9a83c2d84917f5e1",
+    "url": "/static/media/ayushg.9d114b33.JPEG"
+  },
+  {
+    "revision": "e3c441c561ec0b02a9577cffeab9a537",
+    "url": "/static/media/karan.e3c441c5.JPEG"
+  },
+  {
+    "revision": "3f3bf6bf69ba5c2e7b96e1879342c8ea",
+    "url": "/static/media/logo.3f3bf6bf.png"
+  },
+  {
+    "revision": "1ed79fd91977accf8f0fc17b309863c3",
+    "url": "/static/media/rahul.1ed79fd9.JPEG"
+  },
+  {
+    "revision": "b6c6c07652655cf372d0008beb383b66",
+    "url": "/static/media/shataksh.b6c6c076.JPEG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AntescendWebApp/precache-manifest.3d150fdc893c1adc04157821d5ed5749.js"
+  "/precache-manifest.8f9a19b9da27cf5ff79e359a2c236430.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AntescendWebApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
